c56a1a7dd88e4f31ce3ab0c7f58801a072827480
Vitor Rodrigues Danas <vitorrodrigues1207@outlook.com>
2026-09-15 20:36:22 -0300

    Add craft studio static application

---
+++ docs/data.js
@@ -0,0 +1,14 @@
+window.CATALOG={skins:[
+{weapon:'AWP',name:'Duality',image:'assets/awp-duality.png',match:['longevity','dragon'],contrast:['wave'],positions:[[65,37],[62,45],[74,51]]},
+{weapon:'AK-47',name:'Neon Rider',image:'assets/ak-47-neon-rider.png',match:['frog','waveh'],contrast:['crown'],positions:[[61,38],[54,35],[72,42]]},
+{weapon:'M4A1-S',name:'Decimator',image:'assets/m4a1-s-decimator.png',match:['waveh','frog'],contrast:['flame'],positions:[[63,47],[61,57],[73,51]]},
+{weapon:'USP-S',name:'Cortex',image:'assets/usp-s-cortex.png',match:['frog','web'],contrast:['crown'],positions:[[65,37],[73,38],[39,30]]}
+],stickers:[
+{id:'flame',name:'Devouring Flame (Holo)',image:'assets/sticker-devouring-flame-holo.png'},
+{id:'frog',name:'Ultraviolet Poison Frog (Foil)',image:'assets/sticker-ultraviolet-poison-frog-foil.png'},
+{id:'crown',name:'Crown (Foil)',image:'assets/sticker-crown-foil.png'},
+{id:'wave',name:'Great Wave',image:'assets/sticker-great-wave.png'},
+{id:'dragon',name:'Dragon Lore (Foil)',image:'assets/sticker-dragon-lore-foil.png'},
+{id:'waveh',name:'Great Wave (Holo)',image:'assets/sticker-great-wave-holo.png'},
+{id:'web',name:'Web Stuck (Holo)',image:'assets/sticker-web-stuck-holo.png'},
+{id:'longevity',name:'Longevity (Foil)',image:'assets/sticker-longevity-foil.png'}]};
